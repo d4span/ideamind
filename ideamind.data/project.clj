c9e6c521b29f1384fc34b6ee65ac2717ef052265
David Caspar <dcaspar@prodyna.com>
2016-08-13 14:58:21 +0200

    Add inheritance of dependency versions and bump the version of Clojure to 1.9.

--- ideamind.data/project.clj
+++ ideamind.data/project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-modules "0.3.11"]])
+
+  :plugins [[lein-modules "0.3.11"]]
+
+  :dependencies [[org.clojure/clojure :version]])
--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-modules "0.3.11"]])
+
+  :plugins [[lein-modules "0.3.11"]]
+
+  :modules {:versions {org.clojure/clojure "1.9.0-alpha10"}})
